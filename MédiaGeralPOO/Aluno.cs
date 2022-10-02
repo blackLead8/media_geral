df16186be2c4ecfeb7032bb8d24a256d31a77f61
blackLead8 <achristopherdev@gmail.com>
2022-10-01 22:04:09 -0300

    first commit

---
+++ MédiaGeralPOO/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MédiaGeralPOO
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        private double[] _notas;
+        public double Media
+        {
+            get 
+            {
+                return CalcularMedia();
+            }
+        
+        }
+
+        public Aluno(string nome, int provas)
+        {
+            Nome = nome;
+            //O tamanho da array notas será igual ao numero escrito no parametro provas
+            _notas = new double[provas];
+        }
+
+        public void InserirNotas()
+        { 
+            for(int i = 0; i < _notas.Length; i++)
+            {
+                Console.WriteLine("Nota # " + (i + 1) + ": ");
+                _notas[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        private double CalcularMedia()
+        {
+            double total = 0;
+            for(int i = 0; i < _notas.Length; i++)
+            {
+                total += _notas[i];
+            }
+            return total / _notas.Length;
+        }
+
+    }
+}
